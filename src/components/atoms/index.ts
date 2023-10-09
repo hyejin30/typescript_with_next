88b2018893566fe39bd172842d67a5cc2d65962a
hyejin30 <tyui3185@gmail.com>
2023-10-09 16:29:50 +0900

    4.1.1 gitkeep 파일 추가

---
+++ src/components/atoms/index.ts
@@ -0,0 +1,5 @@
+export { default as Button } from "./Button";
+// export { default as Input } from './Input';
+
+// as-is : import { Button } from 'atoms/Button'; import { Input } from 'atoms/Button';
+// to-be : import { Button, Input } from 'atoms';
