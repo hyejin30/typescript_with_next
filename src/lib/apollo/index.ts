414773435a85176261f4d00cd1f04e83ad322ca7
hyejin30 <tyui3185@gmail.com>
2023-10-09 21:03:32 +0900

    4-2. manifest.json 및 graphql code generator 설치

---
+++ src/lib/apollo/index.ts
@@ -0,0 +1,44 @@
+import {
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  NormalizedCacheObject,
+} from "@apollo/client";
+import { useMemo } from "react";
+
+let uri = "/api/graphql";
+let apolloClient: ApolloClient<NormalizedCacheObject> | null = null;
+
+const createApolloClient = () => {
+  return new ApolloClient({
+    ssrMode: typeof window === "undefined",
+    link: new HttpLink({ uri }),
+    cache: new InMemoryCache({}),
+  });
+};
+
+const initApollo = (initialState = {}) => {
+  const client = apolloClient || createApolloClient();
+  // client 인스턴스 중복 생성 방지. 없으면 만든다
+
+  if (initialState) {
+    client.cache.restore({
+      ...client.extract(),
+      ...initialState,
+    });
+  }
+
+  if (typeof window === "undefined") {
+    return client;
+  }
+
+  if (!apolloClient) {
+    apolloClient = client;
+  }
+
+  return client;
+};
+
+export const useApollo = (initialState = {}) => {
+  return useMemo(() => initApollo(initialState), [initialState]);
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
+import { useApollo } from "@/lib/apollo";
 import "@/styles/globals.css";
+import { ApolloProvider } from "@apollo/client";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const apolloClient = useApollo(pageProps.initalApolloState);
+
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
